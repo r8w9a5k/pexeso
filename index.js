c27f8a6a0937a25806f8f6e09e6ad8ec6dc632e0
Jiří Krištof <kristof@ph.infonova.cz>
2021-04-07 22:43:53 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,262 @@
+const express = require("express");
+const { stat } = require("fs");
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const favicon = require("serve-favicon");
+
+app.use("/assets", express.static("assets"));
+app.use("/scripts", express.static("scripts"));
+app.use(favicon(__dirname + "/favicon.ico"));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const FRAME_RATE = 30;
+const BOARD_SIZE = 36;
+const STATE = {
+  NOT_STARTED: 0,
+  ONE_PLAYING: 1,
+  TWO_PLAYING: 2,
+  SHOWING: 3,
+  NO_GAME: 4
+};
+const MOVE_TIME = 10;
+const SHOW_TIME = 2;
+
+const state = {};
+const clientRooms = {};
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+
+  socket.on("newGame", handleNewGame);
+  socket.on("joinGame", handleJoinGame);
+  socket.on("submitMove", handleSubmitMove);
+  socket.on("firstCard", handleFirstCard);
+
+  function handleFirstCard(cardNum) {
+    
+    const roomName = clientRooms[socket.id];
+    if(!roomName) {
+      return;
+    }
+
+    const roomState = state[roomName];
+    if((roomState.state == STATE.ONE_PLAYING && socket.number == 1) ||
+    (roomState.state == STATE.TWO_PLAYING && socket.number == 2)) {
+
+      try {
+
+        const num = parseInt(cardNum);
+
+        if(roomState.board[num] == undefined ||
+          roomState.guessed.includes(num)) {
+          return;
+        }
+  
+        state[roomName].move[0] = num;
+  
+      } catch(e) {
+  
+        console.error(e);
+      }
+    }
+  }
+
+  function handleJoinGame(roomName) {
+
+    const room = io.sockets.adapter.rooms.get(roomName);
+
+    let num_users = 0;
+    if(room) {
+
+      num_users = room.size;
+    }
+
+    if(num_users == 0) {
+      socket.emit("unknownCode");
+      return;
+    } else if(num_users > 1) {
+      socket.emit("tooManyPlayers");
+      return;
+    }
+
+    clientRooms[socket.id] = roomName;
+
+    socket.join(roomName);
+    socket.number = 2;
+    socket.emit("init", 2);
+    socket.emit("gameCode", roomName);
+
+    startGameInterval(roomName);
+  }
+
+  function handleNewGame() {
+
+    const roomName = makeid(4);
+    clientRooms[socket.id] = roomName;
+    socket.emit("gameCode", roomName);
+
+    state[roomName] = initGame();
+
+    socket.join(roomName);
+    socket.number = 1;
+    socket.emit("init", 1);
+  }
+
+  function handleSubmitMove(cardNum) {
+
+    const roomName = clientRooms[socket.id];
+    if(!roomName) {
+      return;
+    }
+    
+    const roomState = state[roomName];
+    if((roomState.state == STATE.ONE_PLAYING && socket.number == 1) ||
+    (roomState.state == STATE.TWO_PLAYING && socket.number == 2)) {
+
+      try {
+
+        const num = parseInt(cardNum);
+
+        if(roomState.board[num] == undefined ||
+          roomState.guessed.includes(num)) {
+          return;
+        }
+
+        roomState.move[1] = num;
+
+        let success = false;
+        if(roomState.board[roomState.move[0]] == roomState.board[roomState.move[1]]) {
+          
+          success = true;
+          roomState.guessed.push(roomState.move[0], roomState.move[1]);
+          roomState.players[socket.number - 1].score++;
+        }
+
+        roomState.timer = 0;
+        roomState.state = STATE.SHOWING;
+        if(!success) {
+          roomState.last = socket.number;
+        }
+      } catch(e) {
+
+        console.error(e);
+      }
+
+    }
+  }
+});
+
+function gameLoop(roomState) {
+
+  roomState.timer++;
+
+  if(roomState.state == STATE.SHOWING && roomState.timer > FRAME_RATE * SHOW_TIME) {
+
+    roomState.timer = 0;
+    roomState.move[0] = undefined;
+    roomState.move[1] = undefined;
+    roomState.state = roomState.last == 2 ? STATE.ONE_PLAYING : STATE.TWO_PLAYING;
+  } else if((roomState.state == STATE.ONE_PLAYING || roomState.state == STATE.TWO_PLAYING) && 
+  roomState.timer > FRAME_RATE * MOVE_TIME) {
+    
+    roomState.move[0] = undefined;
+    roomState.move[1] = undefined;
+    roomState.timer = 0;
+    roomState.state = roomState.state == STATE.ONE_PLAYING ? STATE.TWO_PLAYING : STATE.ONE_PLAYING;
+  }
+
+  return { finished: roomState.guessed.length == roomState.board.length, result: roomState.players };
+}
+
+function startGameInterval(roomName) {
+
+  state[roomName].state = STATE.ONE_PLAYING;
+
+  const interval = setInterval(() => {
+
+    const game = gameLoop(state[roomName]);
+
+    if(!game.finished) {
+      emitGameState(roomName);
+    } else {
+      emitGameState(roomName);
+      emitGameOver(roomName, game.result);
+      clearInterval(interval);
+    }
+  }, 1000 / FRAME_RATE);
+}
+
+function emitGameState(room) {
+
+  const roomState = state[room];
+  const board = Array(BOARD_SIZE).fill(0).map(e => -1);
+  const move = [];
+
+  if(roomState.move[0] != undefined) {
+    //board[roomState.move[0]] = roomState.board[roomState.move[0]];
+    move.push({ pos: roomState.move[0] , val: roomState.board[roomState.move[0]] });
+    if(roomState.move[1] != undefined) {
+      //board[roomState.move[1]] = roomState.board[roomState.move[1]];
+      move.push({ pos: roomState.move[1] , val: roomState.board[roomState.move[1]] });
+    }
+  }
+
+  roomState.guessed.forEach(e => {
+    board[e] = roomState.board[e];
+  });
+  
+  io.to(room).emit("gameState", JSON.stringify({ state: roomState.state, board: board, players: roomState.players, timer: Math.floor(roomState.timer / FRAME_RATE), move: move }));
+}
+
+function emitGameOver(room, result) {
+  io.to(room).emit("gameOver", JSON.stringify({ result }));
+}
+
+function initGame() {
+  const roomState = createGameState();
+  shuffleBoard(roomState);
+  return roomState;
+}
+
+function createGameState() {
+
+  return {
+    timer: 0,
+    state: STATE.NOT_STARTED,
+    move: Array(2),
+    guessed: [],
+    players: [ { score: 0 }, { score: 0 } ],
+    board: Array(BOARD_SIZE).fill(0).map((e, i) => i % (BOARD_SIZE / 2)),
+    last: 0
+  };
+}
+
+function shuffleBoard(state) {
+  const arr = state.board;
+  const len = arr.length;
+  for(let i = len - 1; i > 0; i--) {
+    const j = randint(i);
+    [ arr[i], arr[j] ] = [ arr[j], arr[i] ];
+  }
+}
+
+function makeid(len) {
+  const chars = "abcdefghijklmnpqrstuvwxyz123456789";
+  const chars_len = chars.length;
+  let str = "";
+  for(let i = 0; i < len; i++) {
+    str += chars[randint(chars_len - 1)];
+  }
+  return str;
+}
+
+function randint(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
---
+++ scripts/client.js
@@ -0,0 +1,183 @@
+
+const BOARD_SIZE = 36;
+const MOVE_TIME = 10;
+const SHOW_TIME = 2;
+const EMOJI = ["🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐨","🐯","🦁","🐮","🐷","🐸","🐵","🐥","🦉","🐺"];
+const STATES = {
+	0: "not started",
+	1: "your turn",
+	2: "opponents turn",
+	3: "showing",
+	4: "game finished"
+};
+
+const socket = io();
+let root;
+let playerNum, yourTurn = false, firstCard = true;
+const move = Array(2);
+
+socket.on("init", handleInit);
+socket.on("gameCode", handleGameCode);
+socket.on("unknownCode", handleUnknownCode);
+socket.on("tooManyPlayers", handleTooManyPlayers);
+socket.on("gameState", handleGameState);
+socket.on("gameOver", handleGameOver);
+
+addEventListener("load", () => {
+
+	createBoard();
+	createEvents();
+	hide(id("intro"));
+	hide(id("game"));
+
+});
+
+function handleGameOver(data) {
+	id("status").innerHTML = STATES[4];
+}
+
+function handleInit(num) {
+	playerNum = num;
+	hide(id("menu"));
+	show(id("game"));
+}
+
+function handleGameCode(code) {
+	id("gamecode").innerHTML = "code: " + code;
+}
+
+function handleUnknownCode() {
+	alert("Unknown code");
+}
+
+function handleTooManyPlayers() {
+	alert("Too many players");
+}
+
+function handleGameState(data) {
+	const gameState = JSON.parse(data);
+	fillBoard(gameState.board, gameState.move);
+	let status;
+	if(gameState.state == 1 || gameState.state == 2) {
+		const time = formatTime(MOVE_TIME - gameState.timer);
+		yourTurn = gameState.state == playerNum;
+		if(!yourTurn) {
+			firstCard = true;
+		}
+		status = (yourTurn ? STATES[1] : STATES[2]) + " - " + time;
+	} else if(gameState.state == 3) {
+		yourTurn = false;
+		const time = formatTime(SHOW_TIME - gameState.timer);
+		status = STATES[3] + " - " + time;
+	} else {
+		yourTurn = false;
+		status = STATES[4];
+	}
+	id("status").innerHTML = status;
+	
+	if(id("status").innerHTML.indexOf("your") !== -1) {
+		id("playerYou").style.backgroundColor = 'pink';
+		id("playerOpponent").style.backgroundColor = '#fff';
+	}
+	if(id("status").innerHTML.indexOf("opponents") !== -1) {
+		id("playerYou").style.backgroundColor = '#fff';
+		id("playerOpponent").style.backgroundColor = 'pink';
+	}
+
+	id("playerYou").innerHTML = "<h2>you</h2>" + 
+	"<p>score: " + gameState.players[playerNum - 1].score + "</p>";
+	id("playerOpponent").innerHTML = "<h2>opponent</h2>" + 
+	"<p>score: " + gameState.players[playerNum % 2].score + "</p>";
+}
+
+function formatTime(num) {
+	const fillZero = x => String(x).length == 1 ? "0" + x : x;
+	res = fillZero(num % 60);
+	let temp = Math.floor(num / 60);
+	res = fillZero(temp % 60) + " : " + res;
+	if(temp > 59) {
+		res = Math.floor(temp / 60) + " : " + res;
+	}
+	return res;
+}
+
+function createBoard() {
+	let html = "";
+	for(let i = 0; i < BOARD_SIZE; i++) {
+		html += "<div class='card' id='" + i + "'></div>";
+	}
+	id("board").innerHTML = html;
+}
+
+function createEvents() {
+	if (window.PointerEvent) {                                  /* decent browsers */
+    	addEventListener('pointerdown', handleClick);
+	}
+	else if (window.TouchEvent) {                               /* mobile Safari */
+    	addEventListener('touchstart', handleClick);
+	}
+	else {                                                      /* desktop Safari */
+    	etouch.addEventListener('mousedown', handleClick);
+	}
+	
+	//addEventListener('touchstart', handleClick);
+	addEventListener('gesturestart', e => {
+    		e.preventDefault();
+  	});
+	addEventListener('touchmove', e => {
+    		if(e.scale !== 1) {
+      			e.preventDefault();
+    		}
+  	}, {passive: false});
+	
+	//addEventListener('touchstart', preventZoom); 
+}
+
+function handleClick(e) {
+	const elem = e.target;
+	if(elem.classList.contains("card") && !elem.classList.contains("card-active")) {
+		if(!yourTurn) return;
+		elem.classList.add("card-active");
+		const cardNum = elem.id;
+		if(firstCard) {
+			firstCard = false;
+			move[0] = cardNum;
+			socket.emit("firstCard", cardNum);
+		} else {
+			firstCard = true;
+			yourTurn = false;
+			move[1] = cardNum;
+			socket.emit("submitMove", cardNum);
+		}
+	}
+}
+
+function fillBoard(board, move) {
+	
+	const cards = cl("card");
+	for(let i = 0; i < cards.length; i++) {
+		cards[i].innerHTML = board[i] == -1 ? "" : EMOJI[board[i]];
+		if(move.find(e => e.pos == i)) {
+			cards[i].classList.add("card-active");
+		} else {
+			cards[i].classList.remove("card-active");
+		}
+		if(board[i] == -1) {
+			cards[i].classList.remove("card-guessed");
+		} else {
+			cards[i].classList.add("card-guessed");
+		}
+	}
+	for(let i = 0; i < move.length; i++) {
+		cards[move[i].pos].innerHTML = EMOJI[move[i].val];
+	}
+}
+
+function createGame() {
+	socket.emit("newGame");
+}
+
+function joinGame() {
+	const code = id("code-input").value;
+	socket.emit("joinGame", code);
+}
